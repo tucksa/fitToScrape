1b6ca4950f4219f5f80416294d7581ef2415ec6d
Monty <tuckerjsarah@gmail.com>
2019-04-26 08:43:14 -0400

    h

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const logger = require("morgan");
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => console.log('Mongo is connected'))
   .catch(err => console.log(err));
 
-app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-app.use('/static', express.static(path.join(__dirname, '/app/build/static')));
+//app.use('/static', express.static(path.join(__dirname, '/app/build/static')));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '/app/build/'))
