cd132a79cb2fa0a79ff6f878be8075bf899a9f54
Monty <tuckerjsarah@gmail.com>
2019-04-26 09:10:12 -0400

    heroku

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const logger = require("morgan");
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+const logger = require("morgan");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -18,11 +20,11 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
   .catch(err => console.log(err));
 
 
-// app.use('/static', express.static(path.join(__dirname, '/app/build/static')));
+app.use('/static', express.static(path.join(__dirname, '/app/build/static')));
 
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/app/build/'))
-// });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/app/build/'))
+});
 
 // Use Routes
 app.use('/api', require('./routes/apiRoutes'));
